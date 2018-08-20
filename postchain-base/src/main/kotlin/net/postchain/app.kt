2ef3cbeafc420b46e7770b0b03967524f5ee7392
Eugene Tykulov <eugene.tykulov@gmail.com>
2018-08-20 18:26:14 +0300

    [ConfigurationDataStore]: Interactive mode of CLI has been declined;

--- postchain-base/src/main/kotlin/net/postchain/app.kt
+++ postchain-base/src/main/kotlin/net/postchain/app.kt
@@ -6,29 +6,17 @@ import net.postchain.cli.Cli
 
 
 fun main(args: Array<String>) {
-    while (true) {
-        val cli = Cli()
-        with(readLine()) {
-            when {
-                this == null -> {
-                    cli.usage()
-                }
-
-                !this.isEmpty() -> {
-                    try {
-                        cli.parse(this)
-                    } catch (e: MissingCommandException) {
-                        println(e.message)
-                        cli.usage()
-                    } catch (e: ParameterException) {
-                        println(e.message)
-                        cli.usage(e.jCommander.parsedCommand)
-                    } catch (e: Exception) {
-                        println(e.message)
-                    }
-                }
-
-            }
+    with(Cli()) {
+        try {
+            parse(args)
+        } catch (e: MissingCommandException) {
+            println(e.message)
+            usage()
+        } catch (e: ParameterException) {
+            println(e.message)
+            usage(e.jCommander.parsedCommand)
+        } catch (e: Exception) {
+            println(e.message)
         }
     }
 }
--- postchain-base/src/main/kotlin/net/postchain/cli/Cli.kt
+++ postchain-base/src/main/kotlin/net/postchain/cli/Cli.kt
@@ -1,23 +1,37 @@
 package net.postchain.cli
 
 import com.beust.jcommander.JCommander
+import com.beust.jcommander.MissingCommandException
 
 class Cli {
 
     private val jCommander: JCommander
     private val commands: Map<String, Command> = listOf(
             CommandKeygen()
-            , CommandRunNode()
-            , CommandStopNode()
-            , CommandConfigureNode()
+
             , CommandAddBlockchain()
             , CommandAddConfiguration()
+
+            , CommandConfigureNode()
+            , CommandRunNode()
+//            , CommandStopNode()
+
     ).map { it.key() to it }.toMap()
 
     init {
         jCommander = with(JCommander.newBuilder()) {
             commands.forEach { key, command -> addCommand(key, command) }
             build()
+        }
+    }
+
+    fun parse(args: Array<String>) {
+        jCommander.parse(*args)
+
+        if (jCommander.parsedCommand == null) {
+            throw MissingCommandException("Expected a command, got <no-command>", "<no-command>")
+        } else {
+            commands[jCommander.parsedCommand]?.execute()
         }
     }
 
