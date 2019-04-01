3d4991161a554c2a119402621fc909630e283f05
Duy Chung <formos@FormosVNs-MacBook-Pro.local>
2019-04-01 23:56:20 +0700

    - exclude SAP test in normal profile - only run sap hana test in specific sap-hana profile

--- postchain-base/src/main/kotlin/net/postchain/base/data/PostgreSQLDatabaseAccess.kt
+++ postchain-base/src/main/kotlin/net/postchain/base/data/PostgreSQLDatabaseAccess.kt
@@ -7,11 +7,11 @@ import net.postchain.core.Transaction
 class PostgreSQLDatabaseAccess(sqlCommands: PostgreSQLCommands) : SQLDatabaseAccess(sqlCommands) {
 
     override fun insertBlock(ctx: EContext, height: Long): Long {
-        return queryRunner.insert(ctx.conn, "INSERT INTO blocks (chain_id, block_height) VALUES (?, ?) RETURNING block_iid", longRes, ctx.chainID, height)
+        return queryRunner.query(ctx.conn, "INSERT INTO blocks (chain_id, block_height) VALUES (?, ?) RETURNING block_iid", longRes, ctx.chainID, height)
     }
 
     override fun insertTransaction(ctx: BlockEContext, tx: Transaction): Long {
-        return queryRunner.insert(ctx.conn,
+        return queryRunner.query(ctx.conn,
                 "INSERT INTO transactions (chain_id, tx_rid, tx_data, tx_hash, block_iid) VALUES (?, ?, ?, ?, ?) RETURNING tx_iid",
                 longRes,
                 ctx.chainID, tx.getRID(), tx.getRawData(), tx.getHash(), ctx.blockIID)
