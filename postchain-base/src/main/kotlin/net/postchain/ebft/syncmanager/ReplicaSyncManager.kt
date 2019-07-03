4a6aa7c4892042d60cdb5a0e13e353e4df2daf69
Marko Kranjac <marko@chromaway.com>
2019-07-03 14:50:25 +0200

    [POS-66] factored out fast sync algorithm

--- postchain-base/src/main/kotlin/net/postchain/ebft/syncmanager/ReplicaSyncManager.kt
+++ postchain-base/src/main/kotlin/net/postchain/ebft/syncmanager/ReplicaSyncManager.kt
@@ -13,40 +13,36 @@ import net.postchain.ebft.syncmanager.replica.IssuedRequestTimer
 import net.postchain.ebft.syncmanager.replica.ReplicaTelemetry
 import net.postchain.network.CommunicationManager
 import net.postchain.network.x.XPeerID
+import java.lang.Integer.max
 import java.lang.Integer.min
 import java.util.*
 
-class ReplicaSyncManager(
+class FastSyncAlgorithm(
         signers: List<ByteArray>,
         private val communicationManager: CommunicationManager<EbftMessage>,
-        private val nodeStateTracker: NodeStateTracker,
         private val blockDatabase: BlockDatabase,
-        val blockQueries: BlockQueries,
-        private val blockchainConfiguration: BlockchainConfiguration
-) : SyncManagerBase {
-
+        private val blockchainConfiguration: BlockchainConfiguration,
+        private val replicaTelemetry: ReplicaTelemetry,
+        val blockQueries: BlockQueries
+)
+{
+    private val validatorNodes: List<XPeerID> = signers.map { XPeerID(it) }
     private val parallelism = 10
-    private val nodePoolCount = 1
+    private val nodePoolCount = max(1, signers.count() / 2)
     private val defaultBackoffDelta = 1000
     private val maxBackoffDelta = 30 * defaultBackoffDelta
-    private val validatorNodes: List<XPeerID> = signers.map { XPeerID(it) }
-    private val replicaTelemetry = ReplicaTelemetry()
 
-    private var blockHeight: Long = blockQueries.getBestHeight().get()
     private var nodesWithBlocks = hashMapOf<XPeerID, Long>()
-    private var parallelRequestsState = hashMapOf<Long, IssuedRequestTimer>()
-    private var blocks = PriorityQueue<IncomingBlock>(parallelism)
+    var blockHeight: Long = blockQueries.getBestHeight().get()
+        private set
+    var parallelRequestsState = hashMapOf<Long, IssuedRequestTimer>()
+        private set
+    var blocks = PriorityQueue<IncomingBlock>(parallelism)
+        private set
 
-    override fun update() {
-        replicaTelemetry.logCurrentState(blockHeight, parallelRequestsState, blocks)
-        nodeStateTracker.blockHeight = blockHeight
-        nodeStateTracker.nodeStatuses = replicaTelemetry.nodeStatuses().map { it.serialize() }.toTypedArray()
-        checkBlock()
-        processState()
-        dispatchMessages()
-    }
+    private fun doesQueueContainsBlock(height: Long) = blocks.firstOrNull { it.height == height } != null
 
-    private fun checkBlock() {
+    fun checkBlock() {
         if (blocks.peek() != null) {
             blocks.peek().height.let {
                 when {
@@ -58,23 +54,7 @@ class ReplicaSyncManager(
         }
     }
 
-    private fun commitBlock(block: BlockDataWithWitness) {
-        blockDatabase.addBlock(block)
-                .run {
-                    success {
-                        replicaTelemetry.blockAppendedToDatabase(blockHeight)
-                        parallelRequestsState.remove(blockHeight)
-                        blockHeight += 1
-                        checkBlock()
-                    }
-                    fail {
-                        replicaTelemetry.failedToAppendBlockToDatabase(blockHeight, it.message)
-                        it.printStackTrace()
-                    }
-                }
-    }
-
-    private fun processState() {
+    fun processState() {
         parallelRequestsState.entries.removeIf { it.key <= blockHeight }
         val maxElem = (parallelRequestsState.maxBy { it.key }?.key ?: blockHeight) + 1
         val diff = parallelism - parallelRequestsState.count()
@@ -89,27 +69,7 @@ class ReplicaSyncManager(
         }
     }
 
-    private fun askForBlock(height: Long) {
-        nodesWithBlocks
-                .filter { it.value >= height }
-                .map { it.key }
-                .toMutableList()
-                .also {
-                    it.shuffle()
-                    if (it.count() >= nodePoolCount) {
-                        replicaTelemetry.askForBlock(height, blockHeight)
-                        val timer = parallelRequestsState[height]
-                                ?: IssuedRequestTimer(defaultBackoffDelta, Date().time)
-                        val backoffDelta = min((timer.backoffDelta.toDouble() * 1.1).toInt(), maxBackoffDelta)
-                        communicationManager.sendPacket(GetBlockAtHeight(height), it.first())
-                        parallelRequestsState[height] = timer.copy(backoffDelta = backoffDelta, lastSentTimestamp = Date().time)
-                    }
-                }
-    }
-
-    private fun doesQueueContainsBlock(height: Long) = blocks.firstOrNull { it.height == height } != null
-
-    private fun dispatchMessages() {
+    fun dispatchMessages() {
         for (packet in communicationManager.getPackets()) {
             val xPeerId = packet.first
             val message = packet.second
@@ -139,5 +99,70 @@ class ReplicaSyncManager(
                 replicaTelemetry.fatal("Couldn't handle message $message. Ignoring and continuing", e)
             }
         }
+    }
+
+    private fun commitBlock(block: BlockDataWithWitness) {
+        blockDatabase.addBlock(block)
+                .run {
+                    success {
+                        replicaTelemetry.blockAppendedToDatabase(blockHeight)
+                        parallelRequestsState.remove(blockHeight)
+                        blockHeight += 1
+                        checkBlock()
+                    }
+                    fail {
+                        replicaTelemetry.failedToAppendBlockToDatabase(blockHeight, it.message)
+                        it.printStackTrace()
+                    }
+                }
+    }
+
+    private fun askForBlock(height: Long) {
+        nodesWithBlocks
+                .filter { it.value >= height }
+                .map { it.key }
+                .toMutableList()
+                .also {
+                    it.shuffle()
+                    if (it.count() >= nodePoolCount) {
+                        replicaTelemetry.askForBlock(height, blockHeight)
+                        val timer = parallelRequestsState[height]
+                                ?: IssuedRequestTimer(defaultBackoffDelta, Date().time)
+                        val backoffDelta = min((timer.backoffDelta.toDouble() * 1.1).toInt(), maxBackoffDelta)
+                        communicationManager.sendPacket(GetBlockAtHeight(height), it.first())
+                        parallelRequestsState[height] = timer.copy(backoffDelta = backoffDelta, lastSentTimestamp = Date().time)
+                    }
+                }
+    }
+}
+
+class ReplicaSyncManager(
+        signers: List<ByteArray>,
+        communicationManager: CommunicationManager<EbftMessage>,
+        private val nodeStateTracker: NodeStateTracker,
+        blockDatabase: BlockDatabase,
+        val blockQueries: BlockQueries,
+        blockchainConfiguration: BlockchainConfiguration
+) : SyncManagerBase {
+
+    private val replicaTelemetry = ReplicaTelemetry()
+    private val fastSyncAlgorithm = FastSyncAlgorithm(
+            signers,
+            communicationManager,
+            blockDatabase,
+            blockchainConfiguration,
+            replicaTelemetry,
+            blockQueries
+    )
+
+    override fun update() {
+        replicaTelemetry.logCurrentState(fastSyncAlgorithm.blockHeight, fastSyncAlgorithm.parallelRequestsState, fastSyncAlgorithm.blocks)
+
+        nodeStateTracker.blockHeight = fastSyncAlgorithm.blockHeight
+        nodeStateTracker.nodeStatuses = replicaTelemetry.nodeStatuses().map { it.serialize() }.toTypedArray()
+
+        fastSyncAlgorithm.checkBlock()
+        fastSyncAlgorithm.processState()
+        fastSyncAlgorithm.dispatchMessages()
     }
 }
