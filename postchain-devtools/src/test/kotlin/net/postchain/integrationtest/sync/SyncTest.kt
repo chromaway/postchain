f8ac766678e1f9745b116d8022f1bdae2e9f1f5b
Kalle Rosenbaum <kalle@rosenbaum.se>
2020-12-11 09:03:04 +0100

    Remove duplicate comment

--- postchain-devtools/src/test/kotlin/net/postchain/integrationtest/sync/SyncTest.kt
+++ postchain-devtools/src/test/kotlin/net/postchain/integrationtest/sync/SyncTest.kt
@@ -32,30 +32,6 @@ class SyncTest(val signerCount: Int, val replicaCount: Int, val syncIndex: Set<I
         )
     }
 
-    /*
-    The problem seems to be the same as in other p2p systems: How to find initial peers?
-
-    Problems to solve:
-
-    When a node (old or new) comes online it might not become aware of any currently
-    active nodes because its nodelist might be outdated (network has evolved over time).
-
-    It doesn’t have to happen at blockheight -1. This can happen at any height
-    when the node has been offline for a while. This can only happen on chain0,
-    because that's where all blockchain configurations and nodes are being tracked.
-    As long as chain0 is fully synced, a node should be able to sync any other blockchain
-    in that domain, because it has the most recent signerlist.
-
-    Non-obvious tests to implement:
-
-
-    1 Sync a managed blockchain (not chain0) from newest nodes, where none of the initial nodes exist. (Should work fine)
-    2 Sync chain0 from newest nodes, where none of the initial nodes exist. Should work because newest nodes know about me.
-    3 Test 2, but give it a syncnode via configuration (Should work)
-    4 Sync single sole node from single nodelist (should wait for discovery timeout)
-
-     */
-
     private fun n(index: Int): String {
         var p = nodes[index].pubKey
         return p.substring(0, 4) + ":" + p.substring(64)
