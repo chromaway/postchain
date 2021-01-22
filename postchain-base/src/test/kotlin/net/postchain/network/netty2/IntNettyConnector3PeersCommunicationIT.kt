cced1b174c31639ea7681cbf6adc80247b41196d
Linnéa Rosenbaum <linnea@rosenbaum.se>
2021-01-22 17:03:16 +0100

    Three more blockchainRID size fixes.

--- postchain-base/src/test/kotlin/net/postchain/network/netty2/IntNettyConnector3PeersCommunicationIT.kt
+++ postchain-base/src/test/kotlin/net/postchain/network/netty2/IntNettyConnector3PeersCommunicationIT.kt
@@ -24,7 +24,7 @@ import org.junit.Test
 
 class IntNettyConnector3PeersCommunicationIT {
 
-    private val blockchainRid = BlockchainRid(byteArrayOf(0x01))
+    private val blockchainRid = BlockchainRid.buildRepeat(0x01)
     private lateinit var peerInfo1: PeerInfo
     private lateinit var peerInfo2: PeerInfo
     private lateinit var peerInfo3: PeerInfo
