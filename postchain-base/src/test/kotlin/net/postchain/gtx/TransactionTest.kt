249e7b7ca0fda2230085b7a380c193ba234f5efd
Linnéa Rosenbaum <linnea@rosenbaum.se>
2021-02-04 17:33:01 +0100

    Refactorization of tests according to PR comment

--- postchain-base/src/test/kotlin/net/postchain/gtx/TransactionTest.kt
+++ postchain-base/src/test/kotlin/net/postchain/gtx/TransactionTest.kt
@@ -7,10 +7,12 @@ import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
 import net.postchain.base.BlockchainRid
 import net.postchain.base.SECP256K1CryptoSystem
+import net.postchain.core.Transaction
 import net.postchain.core.Transactor
 import net.postchain.core.TxEContext
 import net.postchain.devtools.KeyPairHelper.privKey
 import net.postchain.devtools.KeyPairHelper.pubKey
+import net.postchain.gtv.Gtv
 import net.postchain.gtv.GtvFactory.gtv
 import net.postchain.gtv.GtvNull
 import org.junit.Assert.assertTrue
@@ -20,60 +22,15 @@ import org.mockito.Mockito.mock
 import java.math.BigInteger
 import kotlin.test.assertFalse
 
-val myCS = SECP256K1CryptoSystem()
-
-fun makeNOPGTX(bcRid: BlockchainRid): ByteArray {
-    val b = GTXDataBuilder(bcRid, arrayOf(pubKey(0)), myCS)
-    b.addOperation("nop", arrayOf(gtv(42)))
-    b.finish()
-    b.sign(myCS.buildSigMaker(pubKey(0), privKey(0)))
-    return b.serialize()
-}
-
-fun makeCorrectNOPGTX(bcRid: BlockchainRid): ByteArray {
-    val b = GTXDataBuilder(bcRid, arrayOf(pubKey(0)), myCS)
-    b.addOperation("nop", arrayOf(gtv(42)))
-    // Need to add a valid dummy operation to make the entire TX valid
-    b.addOperation("gtx_test", arrayOf(gtv(1), gtv("true")))
-    b.finish()
-    b.sign(myCS.buildSigMaker(pubKey(0), privKey(0)))
-    return b.serialize()
-}
-
-fun makeHackerGTXWithTimeB(bcRid: BlockchainRid): ByteArray {
-    val b = GTXDataBuilder(bcRid, arrayOf(pubKey(0)), myCS)
-    val arg0 = -(BigInteger.ONE).shiftLeft(64) //-2^64. A too big value, with first 64 LSB bits = 0
-    b.addOperation("timeb", arrayOf(gtv(arg0), GtvNull))
-    // Need to add a valid dummy operation to make the entire TX valid
-    b.addOperation("gtx_test", arrayOf(gtv(1), gtv("true")))
-    b.finish()
-    b.sign(myCS.buildSigMaker(pubKey(0), privKey(0)))
-    return b.serialize()
-}
-
-fun makeCorrectGTXWithTimeB(bcRid: BlockchainRid): ByteArray {
-    val b = GTXDataBuilder(bcRid, arrayOf(pubKey(0)), myCS)
-    val arg0 = -(BigInteger.ONE).shiftLeft(14) //-2^64. A not too big value
-    b.addOperation("timeb", arrayOf(gtv(arg0), GtvNull))
-    // Need to add a valid dummy operation to make the entire TX valid
-    b.addOperation("gtx_test", arrayOf(gtv(1), gtv("true")))
-    b.finish()
-    b.sign(myCS.buildSigMaker(pubKey(0), privKey(0)))
-    return b.serialize()
-}
-
 class GTXTransactionTest {
 
+    val myCS = SECP256K1CryptoSystem()
     val standardModule = StandardOpsGTXModule()
     val testModule = mock(GTXModule::class.java)
     val compositModule = CompositeGTXModule(arrayOf(standardModule, testModule), false)
 
-    val gtxDataNop = makeNOPGTX(BlockchainRid.EMPTY_RID)
-    val gtxDataCorrectNop = makeCorrectNOPGTX(BlockchainRid.EMPTY_RID)
-    val gtxDataTimebHackerAttaque = makeHackerGTXWithTimeB(BlockchainRid.EMPTY_RID)
-    val gtxDataTimeb = makeCorrectGTXWithTimeB(BlockchainRid.EMPTY_RID)
 
-    class DummyOperation: Transactor {
+    class DummyOperation : Transactor {
         override fun isSpecial() = false
         override fun isCorrect() = true
         override fun apply(ctx: TxEContext) = true
@@ -89,6 +46,8 @@ class GTXTransactionTest {
     @Test
     fun testNopOnly() {
         val factory = GTXTransactionFactory(BlockchainRid.EMPTY_RID, standardModule, myCS)
+        val operationList = listOf(Pair("nop", arrayOf<Gtv>(gtv(42))))
+        val gtxDataNop = makeTx(operationList)
         val tx = factory.decodeTransaction(gtxDataNop)
         assertTrue(tx.getRID().size > 1)
         assertTrue(tx.getRawData().size > 1)
@@ -99,27 +58,42 @@ class GTXTransactionTest {
     @Test
     fun testNopWithOp() {
         val factory = GTXTransactionFactory(BlockchainRid.EMPTY_RID, compositModule, myCS)
+        // gtx_test: Need to add a valid dummy operation to make the entire TX valid
+        val operationList = listOf(Pair("nop", arrayOf<Gtv>(gtv(42))), Pair("gtx_test", arrayOf<Gtv>(gtv(1), gtv("true"))))
+        val gtxDataCorrectNop = makeTx(operationList)
         val tx = factory.decodeTransaction(gtxDataCorrectNop)
-        assertTrue(tx.getRID().size > 1)
-        assertTrue(tx.getRawData().size > 1)
         assertTrue((tx as GTXTransaction).ops.size == 2)
         assertTrue(tx.isCorrect())
     }
 
     @Test(expected = java.lang.ArithmeticException::class)
-    fun testTimeBHackerAttaquetx() {
-        val factory = GTXTransactionFactory(BlockchainRid.EMPTY_RID, compositModule, myCS)
-        val tx = factory.decodeTransaction(gtxDataTimebHackerAttaque)
+    fun testTimeBHackerAttack() {
+        val tx = timeBtransaction(64) //-2^64. A too big value fit in a Long
         tx.isCorrect()
     }
 
     @Test
     fun testTimeBCorrectArgs() {
-        val factory = GTXTransactionFactory(BlockchainRid.EMPTY_RID, compositModule, myCS)
-        val tx = factory.decodeTransaction(gtxDataTimeb)
-        assertTrue(tx.getRID().size > 1)
-        assertTrue(tx.getRawData().size > 1)
-        assertTrue((tx as GTXTransaction).ops.size == 2)
+        val tx = timeBtransaction(14) //-2^14 is a not too big value for a Long
         assertTrue(tx.isCorrect())
+    }
+
+    private fun makeTx(opList: List<Pair<String, Array<Gtv>>>): ByteArray {
+        val b = GTXDataBuilder(BlockchainRid.EMPTY_RID, arrayOf(pubKey(0)), myCS)
+        for (operation in opList) {
+            b.addOperation(operation.first, operation.second)
+        }
+        b.finish()
+        b.sign(myCS.buildSigMaker(pubKey(0), privKey(0)))
+        return b.serialize()
+    }
+
+    private fun timeBtransaction(i: Int): Transaction {
+        val factory = GTXTransactionFactory(BlockchainRid.EMPTY_RID, compositModule, myCS)
+        val arg0 = -(BigInteger.ONE).shiftLeft(i) //-2^i. A not too big value
+        // gtx_test: Need to add a valid dummy operation to make the entire TX valid
+        val operationList = listOf(Pair("timeb", arrayOf<Gtv>(gtv(arg0), GtvNull)), Pair("gtx_test", arrayOf<Gtv>(gtv(1), gtv("true"))))
+        val gtxDataTimeb = makeTx(operationList)
+        return factory.decodeTransaction(gtxDataTimeb)
     }
 }
