b8490a24bb67802c03a7462b937c4d8b323f682b
Duy Chung <chungkhanhduy1985@gmail.com>
2021-05-25 23:09:08 +0700

    remove sap hana support

--- postchain-base/src/main/kotlin/net/postchain/base/data/DatabaseAccessFactory.kt
+++ postchain-base/src/main/kotlin/net/postchain/base/data/DatabaseAccessFactory.kt
@@ -3,12 +3,10 @@ package net.postchain.base.data
 object DatabaseAccessFactory {
 
     const val POSTGRES_DRIVER_CLASS = "org.postgresql.Driver"
-    const val SAP_HANA_DRIVER_CLASS = "com.sap.db.jdbc.Driver"
 
     fun createDatabaseAccess(driverClassName: String): DatabaseAccess {
         return when (driverClassName) {
             POSTGRES_DRIVER_CLASS -> PostgreSQLDatabaseAccess()
-            SAP_HANA_DRIVER_CLASS -> SAPHanaSQLDatabaseAccess()
             else -> throw Exception("Unknown database driver class detected: $driverClassName")
         }
     }
