de18d3141ed5efa3daa402474e5bc3aededacd85
Alex Mizrahi <alex.mizrahi@gmail.com>
2021-06-24 14:39:45 +0300

    fixed block tx count issue, kotlin syntax update

--- postchain-base/src/main/kotlin/net/postchain/base/data/BaseBlockBuilder.kt
+++ postchain-base/src/main/kotlin/net/postchain/base/data/BaseBlockBuilder.kt
@@ -293,14 +293,14 @@ open class BaseBlockBuilder(
     }
 
     override fun appendTransaction(tx: Transaction) {
-        checkSpecialTransaction(tx) // note: we check even transactions we construct ourselves
-        super.appendTransaction(tx)
-        blockSize += tx.getRawData().size
-        if (blockSize >= maxBlockSize) {
+        if (blockSize + tx.getRawData().size > maxBlockSize) {
             throw BlockValidationMistake("block size exceeds max block size ${maxBlockSize} bytes")
         } else if (transactions.size >= maxBlockTransactions) {
             throw BlockValidationMistake("Number of transactions exceeds max ${maxBlockTransactions} transactions in block")
         }
+        checkSpecialTransaction(tx) // note: we check even transactions we construct ourselves
+        super.appendTransaction(tx)
+        blockSize += tx.getRawData().size
     }
 
 }
